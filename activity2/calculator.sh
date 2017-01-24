a90e2b72856744c387a2281488a041dfc4c2281d
Damien Robinson <damien.robinson@griffithuni.edu.au>
2017-01-23 20:45:42 -0800

    Change the name of the activity 2 folder to be called activity2.

---
+++ activity2/calculator.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+# Read in two numbers seperated by a space.
+echo "Enter two numbers: "
+read numbers
+
+# Read in an operation to be preformed on the given numbers.
+echo "Enter the operation: "
+read operation
+
+case operation in
+	add)
+		add $numbers
+		;;
+	sub)
+		sub $numbers
+		;;
+	mul)
+		mul $numbers
+		;;
+	div)
+		div $numbers
+		;;
+esac
+	
+
+# Add function preforms the add operation on two given numbers.
+Add (){
+	echo expr $1 + $2
+}
+
+# Subtract function preforms the stubtraction operation on two given numbers.
+Subtract (){
+	echo expr $1 - $2
+}
+
+# Multiply function preforms the multiplication operation on two given numbers.
+Multiply (){
+	echo expr $1 * $2
+}
+
+# Divide function preforms the division operation on two given numbers.
+Divide (){
+	echo expr $1 + $2
+}
