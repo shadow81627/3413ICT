ed6deb5c7a04880a7b06730d8be63d9a9b904672
Damien Robinson <damien.robinson@griffithuni.edu.au>
2017-01-23 20:16:08 -0800

    Added a script that queries for 2 numbers and an operation to be preformed on those two numbers.

---
+++ Activity 2/calculator.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Read in two numbers seperated by a space.
+echo "Enter two numbers: "
+read numbers
+
+# Read in an operation to be preformed on the given numbers.
+echo "Enter the operation: "
+read operation
+
