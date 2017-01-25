8b7406597313b1e2ef82c9e617ea705b8ffa85c5
Damien Robinson <shadow81627@gmail.com>
2017-01-24 19:29:04 -0800

    Added a script that Reads the input from a user and a file and compares the user input to the file input, if the input matches then the line in the file will be displayed.

---
+++ weather.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Reads the input from a user and a file and compares the user input to the file input, 
+# if the input matches then the line in the file will be displayed.
+
+# Read in a weather condition.
+echo "Enter weather condition (sunny, rainy, windy, foggy):  "
+read input
+
+
