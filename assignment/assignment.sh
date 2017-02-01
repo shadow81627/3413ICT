64ac457600a16221099eaa4630df59d9d52bf9d6
Damien <shadow81627@gmail.com>
2017-01-31 20:25:09 -0800

    Added stub for the create function.

--- assignment/assignment.sh
+++ assignment/assignment.sh
@@ -1,7 +1,22 @@
 #!/bin/sh
 
+# Create a verification file
+create(){
+	
+}
+
+# Choose which action to prefrom based on user input
 while [ "$0" != "" ]; do
 	case $0 in
+		# Creates a new verifcation file
 		-c | --create)
 			shift
 			filename=$0
+			;;
+		
+		# Exit the script when unknown user input.
+		*)
+			exit
+			;;
+	esac
+done
