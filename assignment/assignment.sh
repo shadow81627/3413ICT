331b1ead1d4200a0e4a51d9a838adad47971d0dd
Damien <shadow81627@gmail.com>
2017-01-31 21:11:23 -0800

    Added a check to see if there is a file anme when creating a verification file.

--- assignment/assignment.sh
+++ assignment/assignment.sh
@@ -2,20 +2,41 @@
 
 # Create a verification file
 create(){
-	
+	touch $1
+	echo "$1"
+
+
+	ls -l
+	echo "File Created"
+}
+
+# Verify a verification file
+verify(){
+	echo ""
 }
 
 # Choose which action to prefrom based on user input
-while [ "$0" != "" ]; do
-	case $0 in
-		# Creates a new verifcation file
+while [ "$1" != "" ]; do
+	case $1 in
+		# Command to create a new file
 		-c | --create)
 			shift
-			filename=$0
+			if [ "$1" != "" ] 
+			then
+				create $1
+			else
+				echo "pls enter file"
+			fi
+			;;
+		# Command for file validation
+		-v | --verify)
+			shift
+			verify $1
 			;;
 		
 		# Exit the script when unknown user input.
 		*)
+			echo "Unknown input."
 			exit
 			;;
 	esac
