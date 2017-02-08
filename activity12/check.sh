03593ed3da48a1bf8d131709f3e2ee6666ac7f33
Damien Robinson <shadow81627@gmail.com>
2017-02-07 19:22:37 -0800

    Fixed indenting and added comments to the check.sh file.

--- activity12/check.sh
+++ activity12/check.sh
@@ -1,15 +1,19 @@
 #!/bin/sh
 
+# Save attributes of passwd file into two different varibles
 old=`ls -l /etc/passwd`
 new=`ls -l /etc/passwd`
 
+# While loop will continue until the variables get different value
 while [ "$old" = "$new" ]
 do
 
-    ./vulp<passwd_input
-    new=`ls -l /etc/passwd`
-    counter=`expr $counter + 1`
-    echo "$counter\n"
+	./vulp<passwd_input
+	# Add a new user Eve to the passwd file
+	new=`ls -l /etc/passwd`
+	counter=`expr $counter + 1`
+	echo "$counter\n"
+
 done
 
 echo "STOP... The file has been changed"
