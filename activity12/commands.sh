24f456604ca1b382dcf3af328aaf0b90d4038810
Damien Robinson <shadow81627@gmail.com>
2017-02-07 19:16:27 -0800

    Added the passwd_inpute file.

--- activity12/commands.sh
+++ activity12/commands.sh
@@ -6,3 +6,11 @@ sudo sysctl -w kernel.yama.protected_sticky_symlinks=0
 # Compile the vulp file
 gcc vulp.c -o vulp
 
+# Allows the users to exec a program with the permissions of its owner.
+sudo chown root vulp
+
+# Give the file super user privilage
+sudo chmod u+s vulp
+
+# List the vulp attributes
+ll vulp
