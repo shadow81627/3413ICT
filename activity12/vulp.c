0ed426c4ca4fad31c1584deedfca11251a7c72e1
Damien Robinson <shadow81627@gmail.com>
2017-02-07 17:23:33 -0800

    Fixed indenting and added comments for vulp.

--- activity12/vulp.c
+++ activity12/vulp.c
@@ -1,24 +1,27 @@
-    /*  vulp.c  */
+/*  vulp.c  */
 
-    #include <stdio.h>
-    #include <unistd.h>
-    #include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
 
-    int main()
-    {
-       char * fn = "/tmp/std1";
-       char buffer[60];
-       FILE *fp;
-       
-       /* get user input */
-       scanf("%50s", buffer );
-       if(!access(fn, W_OK)){
+int main()
+{
+	// Target File
+	char * fn = "/tmp/std1";
+	char buffer[60];
+	FILE *fp;
 
-            fp = fopen(fn, "a+");
-            fwrite("\n", sizeof(char), 1, fp);
-            fwrite(buffer, sizeof(char), strlen(buffer), fp);
-            fclose(fp);
-            printf("Done!\n");
-       }
-       else printf("No permission \n");
-    }
+	/* get user input */
+	// Read from passwd_input or shadow_input file
+	scanf("%50s", buffer );
+	if(!access(fn, W_OK)){
+
+		fp = fopen(fn, "a+");
+		//Add a new user to the file if access is gianed
+		fwrite("\n", sizeof(char), 1, fp); 
+		fwrite(buffer, sizeof(char), strlen(buffer), fp);
+		fclose(fp);
+		printf("Done!\n");
+	}
+	else printf("No permission \n");
+}
