47ef0983a7bd8d9db6f692700538d51e625cfcfe
Damien Robinson <shadow81627@gmail.com>
2017-02-08 21:18:02 -0800

    Added code to get the file attributes.

--- assignment/assignment.sh
+++ assignment/assignment.sh
@@ -8,8 +8,22 @@ create(){
 
 	ls -l | while read line
 	do
-		echo "$line"  > $1
-		echo $line | awk '/a/{$md5=md5sum $8} END {print md5}' >> $1
+		#echo "$line"  > $1
+		#echo $line | awk '/a/{$md5=md5sum $8} END {print md5}' >> $1
+		
+		# The name of the file
+		filename=`echo $line | awk '{ print $9 }'`
+		# The file path where the file is located
+		#filepath=readlink -f $filename
+		# The access permissions for user groups
+		access=`echo $line | awk '{ print $1 }'`
+		# The type of file
+		filetype=`echo $access | awk '{print substr($0,0,1)}'`
+		owner=`echo $line | awk '{ print $3 }'`
+		group=`echo $line | awk '{ print $4 }'`
+		date=`echo $line | awk '{ print $6, $7, $8 }'`
+		
+		echo "$filepath $filename $filetype $access $owner $group $date"
 		
 	done
 	echo "File Created."
